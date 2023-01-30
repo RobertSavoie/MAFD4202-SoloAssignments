e5c26a847e9db47a88c212252fbc797d6bc935e0
Robert Savoie <robert.savoie1@dcmail.ca>
2023-01-29 23:19:13 -0500

    Updating logic. Transport charge not quite working properly yet

--- A2-ItemList/A2-ItemList.cbl
+++ A2-ItemList/A2-ItemList.cbl
@@ -50,7 +50,7 @@
            05 ws-eof-other             pic x value "x".
       *
        01 ws-name.
-           05 filler                   pic x(161).
+           05 filler                   pic x(158).
            05 filler                   pic x(14) value "ROB SAVOIE, A2".
       *
        01 ws-heading.
@@ -71,11 +71,11 @@
            05 filler                   pic x(5) value "  |  ".
            05 ws-head-eight            pic x(13) value "Product Class".
            05 filler                   pic x(5) value "  |  ".
-           05 ws-head-nine             pic x(16) value
-                                       "Transportation %".
+           05 ws-head-nine             pic x(7) value
+                                       "Trans %".
            05 filler                   pic x(5) value "  |  ".
-           05 ws-head-ten              pic x(21) value
-                                       "Transportation Charge".
+           05 ws-head-ten              pic x(12) value
+                                       "Trans Charge".
       *
        01 ws-general.
            05 filler                   pic xxx value spaces.
@@ -84,20 +84,26 @@
            05 ws-desc                  pic x(11).
            05 filler                   pic x(5) value spaces.
            05 ws-qty                   pic zzz.
-           05 filler                   pic x(8) value spaces.
+           05 filler                   pic x(7) value spaces.
            05 ws-price-per-unit        pic z,zzz,zz9.99.
-           05 filler                   pic x(5) value spaces.
+           05 filler                   pic x(7) value spaces.
            05 ws-ext-price             pic z,zzz,zz9.99.
-           05 filler                   pic x(5) value spaces.
-           05 ws-discount              pic 9(15).
-           05 filler                   pic x(5) value spaces.
-           05 ws-net-price             pic 9(9).
-           05 filler                   pic x(12) value spaces.
-           05 ws-product-class         pic x(13).
+           05 filler                   pic x(8) value spaces.
+           05 ws-discount-amount       pic z,zzz,zz9.99.
            05 filler                   pic x(2) value spaces.
-           05 ws-trans-percent         pic 9(16).
-           05 filler                   pic x(5) value spaces.
-           05 ws--trans-charge         pic 9(18).
+           05 ws-net-price             pic z,zzz,zz9.99.
+           05 filler                   pic x(11) value spaces.
+           05 ws-product-class         pic x(13).
+           05 filler                   pic x(3) value spaces.
+           05 ws-trans-percent         pic z9.9.
+           05 filler                   pic xxx value " % ".
+           05 ws-trans-charge          pic z,zzz,zz9.99.
+      *
+       01 ws-math-store.
+           05 ws-store-ext             pic 9(10).
+           05 ws-store-trans           pic 9(10)v9999.
+           05 ws-store-discount        pic 9(10).
+           05 ws-store-net             pic 9(10).
       *
        01 ws-summary.
            05 filler                   pic xxx value spaces.
@@ -106,14 +112,20 @@
            05 filler                   pic xxx value spaces.
       *
        01 ws-cnsts.
-           05 ws-discount-a            pic 9.999 value 1.125.
-           05 ws-discount-d            pic 9.99 value 1.85.
-           05 ws-discount-f            pic 9.99 value 1.45.
-           05 ws-trans-percent         pic 9.99 value 1.65.
+           05 ws-transport-A           pic 99v9 value 12.5.
+           05 ws-transport-B           pic 9v9 value 8.5.
+           05 ws-transport-F           pic 9v9 value 4.5.
+           05 ws-transport-default     pic 9v9 value 6.5.
            05 ws-trans-cost            pic 99 value 45.
+           05 ws-discount              pic 9v99 value 0.05.
            05 ws-class-A               pic x value "A".
            05 ws-class-B               pic x value "B".
            05 ws-class-F               pic x value "F".
+           05 ws-percent-A             pic 9v999 value 0.125.
+           05 ws-percent-B             pic 9v99 value 0.85.
+           05 ws-percent-F             pic 9v99 value 0.45.
+           05 ws-percent-default       pic 9v99 value 0.65.
+
       *
        procedure division.
       *
@@ -139,23 +151,81 @@
       *
       *clear output buffer
       *
-           move spaces to output-line.
-      *
-      *move detail output data
-      *
-           move il-item-number     to ws-item-number.
-           move il-desc            to ws-desc.
-           move il-qty             to ws-qty.
-           move il-price-per-unit  to ws-price-per-unit.
-           move il-price-per-unit  to ws-price-per-unit.
-           move il-product-class   to ws-product-class.
+           move spaces to ws-general.
       *
       *extended price
       *
            multiply il-qty
                  by il-price-per-unit
-             giving ws-ext-price.
+             giving ws-store-ext.
+
       *
+      *move detail output data
+           move il-item-number     to ws-item-number.
+           move il-desc            to ws-desc.
+           move il-qty             to ws-qty.
+           move il-price-per-unit  to ws-price-per-unit.
+           move il-product-class   to ws-product-class.
+      *
+      *if statements
+      *
+           if (ws-store-ext is greater than 100 and ws-product-class is
+               equal to ws-class-A) then
+               multiply ws-store-ext
+                 by     ws-discount
+                 giving ws-store-discount
+           else if (ws-store-ext is greater than 50 and ws-product-class
+           is equal to ws-class-F) then
+               multiply ws-store-ext
+                     by ws-discount
+                 giving ws-store-discount
+           else if (ws-product-class is equal to ws-class-B and il-qty 
+           is greater than 5) then
+               multiply ws-store-ext
+                     by ws-discount
+                 giving ws-store-discount
+           else 
+               move 0.0 to ws-store-discount
+           end-if.
+      *
+           if (ws-product-class is equal to ws-class-A) then
+               move ws-transport-A to ws-trans-percent
+               multiply ws-percent-A
+                     by ws-store-net
+                 giving ws-trans-charge
+      *
+           else if (ws-product-class is equal to ws-class-B) then
+               move ws-transport-B to ws-trans-percent
+               multiply ws-percent-B
+                     by ws-store-net
+                 giving ws-trans-charge
+      *
+           else if (ws-product-class is equal to ws-class-F) then
+               move ws-transport-F to ws-trans-percent
+               multiply ws-percent-F
+                     by ws-store-net
+                 giving ws-trans-charge
+      *
+           else if (ws-qty is less than or equal to 100) then
+               move ws-transport-default to ws-trans-percent
+               multiply ws-percent-default
+                     by ws-store-net
+                 giving ws-trans-charge
+      *
+           else
+               move 0.0 to ws-trans-percent
+               add 45
+                to ws-store-net
+            giving ws-net-price
+           end-if.
+      *    
+              add ws-store-discount
+               to ws-store-ext
+           giving ws-store-net.
+      *
+           move ws-store-discount  to ws-discount-amount.
+           move ws-store-ext       to ws-ext-price.
+           move ws-store-net       to ws-net-price.
       *write detail output
       *
            write output-line from ws-general
