583724e41396cf52b475da2010162e8ec8e5ceb4
Robert Savoie <robert.savoie1@dcmail.ca>
2023-02-16 09:36:54 -0500

    Added header stuff

--- A3-SalesComm/A3-SalesComm.cbl
+++ A3-SalesComm/A3-SalesComm.cbl
@@ -39,32 +39,63 @@
       *
        working-storage section.
       *
-       01 ws-eof-flag                  pic x value 'n'.
+       01 ws-heading-name-line.
+           05 filler                   pic x(63) value spaces.
+           05 ws_username              pic x(27) value
+                                       "Robert Savoie, Assignment 3".
       *
-       01 ws-heading1-name-line.
-
+       01 ws-heading-title.
+           05 filler                   pic x(34) value spaces.
+           05 filler                   pic x(23) value
+                                       "SALES COMMISSION REPORT".
+           05 filler                   pic x(33) value spaces.
       *
-       01 ws-heading2-title.
-
+       01 ws-heading-headings.
+           05 filler                   pic x(3)  value "NO.".
+           05 filler                   pic x(5)  value spaces.
+           05 filler                   pic x(4)  value "NAME".
+           05 filler                   pic x(6)  value spaces.
+           05 filler                   pic x(5)  value "SALES".
+           05 filler                   pic x(5)  value spaces.
+           05 filler                   pic x(3)  value "MIN".
+           05 filler                   pic x(6)  value spaces.
+           05 filler                   pic x(3)  value "MAX".
+           05 filler                   pic x(4)  value spaces.
+           05 filler                   pic x(4)  value "RATE".
+           05 filler                   pic x(5)  value spaces.
+           05 filler                   pic x(6)  value "EARNED".
+           05 filler                   pic x(6)  value spaces.
+           05 filler                   pic x(4)  value "PAID".
+           05 filler                   pic x(5)  value spaces.
+           05 filler                   pic x(14) value "BONUS/NO BONUS".
+           05 filler                   pic x(2)  value spaces.
       *
-       01 ws-heading3-headings.
-
+       01 ws-heading-underlines.
+           05 filler                   pic x(3)  value "---".
+           05 filler                   pic x(3)  value spaces.
+           05 filler                   pic x(8)  value "--------".
+           05 filler                   pic x(3)  value spaces.
+           05 filler                   pic x(7)  value "-------".
+           05 filler                   pic x(2)  value spaces.
+           05 filler                   pic x(7)  value "-------".
+           05 filler                   pic x(2)  value spaces.
+           05 filler                   pic x(7)  value "-------".
+           05 filler                   pic x(2)  value spaces.
+           05 filler                   pic x(5)  value "-----".
+           05 filler                   pic x(1)  value spaces.
+           05 filler                   pic x(10) value "----------".
+           05 filler                   pic x(2)  value spaces.
+           05 filler                   pic x(10) value "----------".
+           05 filler                   pic x(2)  value spaces.
+           05 filler                   pic x(16) value
+                                       "----------------".
       *
-       01 ws-heading4-underlines.
-
+      *eof constants
+           77 ws-eof-flag                  pic x value "n".
+           77 ws-eof-Y                     pic x value "y".
+           77 ws-eof-N                     pic x value "n".
       *
-       01 ws-report-detail-line.
-
-      *
-       01 ws-total-line.
-
-      *
-       01 ws-num-max-line.
-
-      *
-       01 ws-num-min-line.
-
-      *
+      *constants
 
       *
        procedure division.
@@ -84,7 +115,27 @@
       *
 
       *
-       300-print-totals.
+       350-print-totals.
+      *
+
+      *
+       400-bonus-greater-than.
+      *
+
+      *
+       500-bonus-less-than.
+      *
+
+      *
+       600-bonus-under-minimum.
+      *
+
+      *
+       700-bonus-over-maximum.
+      *
+
+      *
+       800-print-totals.
       *
 
       *
