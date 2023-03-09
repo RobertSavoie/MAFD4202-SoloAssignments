7ec60502e597920405c06e06f05ed366670e4d51
Robert Savoie <robert.savoie1@dcmail.ca>
2023-03-09 14:14:32 -0500

    Formatted output

--- A4-SalaryReport/A4-SalaryReport.cbl
+++ A4-SalaryReport/A4-SalaryReport.cbl
@@ -55,9 +55,100 @@
            05 ws-page-number           pic z9.
            05 filler                   pic x(6).
       *
+       01 ws-column-head-one.
+           05 filler                   pic x       value spaces.
+           05 filler                   pic xxx     value "EMP".
+           05 filler                   pic xx      value spaces.
+           05 filler                   pic xxx     value "EMP".
+           05 filler                   pic x(28)   value spaces.
+           05 filler                   pic x(7)    value "PRESENT".
+           05 filler                   pic xx      value spaces.
+           05 filler                   pic x(8)    value "INCREASE".
+           05 filler                   pic x(5)    value spaces.
+           05 filler                   pic xxx     value "PAY".
+           05 filler                   pic x(11)   value spaces.
+           05 filler                   pic xxx     value "NEW".
+           05 filler                   pic x(4)    value spaces.
+      *
+       01 ws-column-head-two.
+           05 filler                   pic x       value spaces.
+           05 filler                   pic xxx     value "NUM".
+           05 filler                   pic xx      value spaces.
+           05 filler                   pic x(4)    value "NAME".
+           05 filler                   pic x(10)   value spaces.
+           05 filler                   pic x(5)    value "YEARS".
+           05 filler                   pic x       value spaces.
+           05 filler                   pic x(8)    value "POSITION".
+           05 filler                   pic x(4)    value spaces.
+           05 filler                   pic x(6)    value "SALARY".
+           05 filler                   pic x(5)    value spaces.
+           05 filler                   pic x       value "%".
+           05 filler                   pic x(7)    value spaces.
+           05 filler                   pic x(8)    value "INCREASE".
+           05 filler                   pic x(7)    value spaces.
+           05 filler                   pic x(6)    value "SALARY".
+           05 filler                   pic xx      value spaces.
+      *
+       01 ws-print-line.
+           05 filler                   pic x       value spaces.
+           05 ws-emp-num               pic xxx     value spaces.
+           05 filler                   pic x       value spaces.
+           05 ws-emp-name              pic x(15)   value spaces.
+           05 filler                   pic xx      value spaces.
+           05 ws-emp-year              pic z9.
+           05 filler                   pic xx      value spaces.
+           05 ws-emp-position          pic x(8)    value spaces.
+           05 filler                   pic xx      value spaces.
+           05 ws-emp-pres-salary       pic zz,zz9.99.
+           05 filler                   pic xx      value spaces.
+           05 ws-emp-increase-perc     pic z9.9.
+           05 filler                   pic x       value "%".
+           05 filler                   pic xxx     value spaces.
+           05 ws-emp-increase-pay      pic $$$,$$$.99.
+           05 filler                   pic x       value "+".
+           05 filler                   pic x       value spaces.
+           05 filler                   pic x       value "$".
+           05 filler                   pic xx      value spaces.
+           05 ws-emp-new-salary        pic zzz,zz9.99.
+      *
+       01 ws-class-heading.
+           05 filler                   pic x       value spaces.
+           05 filler                   pic x(15)   value
+                                       "EMPLOYEE CLASS:".
+           05 filler                   pic x(8)    value spaces.
+           05 filler                   pic x(7)    value "Analyst".
+           05 filler                   pic x(4)    value spaces.
+           05 filler                   pic x(8)    value "Sen Prog".
+           05 filler                   pic x(4)    value spaces.
+           05 filler                   pic x(4)    value "Prog".
+           05 filler                   pic x(4)    value spaces.
+           05 filler                   pic x(7)    value "Jr Prog".
+           05 filler                   pic x(4)    value spaces.
+           05 filler                   pic x(12)   value "Unclassified".
+           05 filler                   pic x(2)    value spaces.
+      *
+       01 ws-class-totals.
+           05 filler                   pic x       value spaces.
+           05 filler                   pic x(15)   value
+                                       "# ON THIS PAGE:".
+           05 filler                   pic x(13)   value spaces.
+           05 ws-analyst-total         pic z9      value 0.
+           05 filler                   pic x(10)   value spaces.
+           05 ws-sen-prog-total        pic z9      value 0.
+           05 filler                   pic x(6)    value spaces.
+           05 ws-prog-total            pic z9      value 0.
+           05 filler                   pic x(9)    value spaces.
+           05 ws-jrprog-total          pic z9      value 0.
+           05 filler                   pic x(14)   value spaces.
+           05 ws-unclassified-total    pic z9      value 0.
+           05 filler                   pic xx      value spaces.
+      *
+       01 ws-math-store.
+           05 ws-new-salary            pic 9(6).
+      *
        01 ws-counters.
-           05 ws-cntr-page             pic 99 value 0.
-           05 ws-cntr-line             pic 99 value 0.
+           05 ws-cntr-page             pic 99      value 0.
+           05 ws-cntr-line             pic 99      value 0.
       *
       *eof constants
        77 ws-eof-flag                  pic x       value "n".
@@ -77,7 +168,8 @@
       *
            perform 100-print-report-heading.
       *
-           perform 200-process-pages.
+           perform 200-process-pages
+             until ws-eof-flag equals ws-eof-Y.
       *
            perform 75-close-files.
       *
@@ -122,17 +214,49 @@
                  from ws-page-heading
                  after advancing 2 lines
            end-if.
+           write output-line
+             from ws-column-head-one
+             after advancing 2 lines.
+           write output-line
+             from ws-column-head-two
+             before advancing 2 lines.
+      *
+       150-clear-artifacts.
+      *
+           move spaces to ws-print-line.
+           move spaces to ws-math-store.
       *
        200-process-pages.
       *
            perform 125-print-page-headings.
            perform 250-process-lines
              varying ws-cntr-line from 0 by 1
-             until ws-cntr-line > ws-lines-per-page
-             or ws-eof-flag = ws-eof-Y.
+             until ws-cntr-line greater than ws-lines-per-page
+             or ws-eof-flag equals ws-eof-Y.
+           perform 650-print-totals.
       *
        250-process-lines.
       *
-
+           perform 600-create-output-line.
+           perform 50-read-input-file.
+      *
+       600-create-output-line.
+      *
+           move il-emp-num         to ws-emp-num.
+           move il-emp-name        to ws-emp-name.
+           move il-emp-years       to ws-emp-year.
+           move il-emp-sal         to ws-emp-pres-salary.
+           write output-line
+             from ws-print-line
+             before advancing 1 line.
+      *
+       650-print-totals.
+      *
+           write output-line
+             from ws-class-heading
+             after advancing 1 line.
+           write output-line
+             from ws-class-totals
+             before advancing 2 lines.
       *
        end program A4-SalaryReport.
