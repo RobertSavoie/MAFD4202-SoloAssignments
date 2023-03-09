fd9259722f5afb7cd380fa4f0ca3ab84d94675ad
Robert Savoie <robert.savoie1@dcmail.ca>
2023-03-09 18:53:34 -0500

    Program 99% finished. Still need to figure out how to get the increase % slow to be blank instead of 0.0%

--- A4-SalaryReport/A4-SalaryReport.cbl
+++ A4-SalaryReport/A4-SalaryReport.cbl
@@ -144,31 +144,86 @@
            05 ws-unclassified-total    pic z9      value 0.
            05 filler                   pic xx      value spaces.
       *
-       01 ws-math-store.
-           05 ws-new-salary            pic 9(6).
+       01 ws-increase-average-one.
+           05 filler                   pic x       value spaces.
+           05 filler                   pic x(18)   value 
+                                       "AVERAGE INCREASES:".
+           05 filler                   pic xxx     value spaces.
+           05 filler                   pic x(8)    value "ANALYST=".
+           05 filler                   pic x(5)    value spaces.
+           05 ws-analyst-average       pic z,zz9.99
+                                                   value 0.
+           05 filler                   pic x(5)    value spaces.
+           05 filler                   pic x(9)    value "SEN PROG=".
+           05 filler                   pic xxx     value spaces.
+           05 ws-senprog-average       pic z,zz9.99
+                                                   value 0.
+           05 filler                   pic x(12)   value spaces.
       *
-       01 ws-counters.
-           05 ws-cntr-page             pic 99      value 0.
-           05 ws-cntr-line             pic 99      value 0.
+       01 ws-increase-average-two.
+           05 filler                   pic x(22)   value spaces.
+           05 filler                   pic x(5)    value "PROG=".
+           05 filler                   pic x(8)    value spaces.
+           05 ws-prog-average          pic z,zz9.99
+                                                   value 0.
+           05 filler                   pic x(5)    value spaces.
+           05 filler                   pic x(8)    value "JR PROG=".
+           05 filler                   pic x(4)    value spaces.
+           05 ws-jrprog-average        pic z,zz9.99
+                                                   value 0.
+           05 filler                   pic x(12)   value spaces.
+      *
+       01 ws-math-store.
+           05 ws-math-increase-pay     pic 9(9)v99.
+           05 ws-math-new-salary       pic 9(9)v99.
+           05 ws-math-average          pic 9(9)v99.
+           05 ws-math-percent          pic 9v999.
+      *
+       01 ws-math-totals.
+           05 ws-math-analyst-total    pic 9(7)v9(4).
+           05 ws-math-senprog-total    pic 9(7)v9(4).
+           05 ws-math-prog-total       pic 9(7)v9(4).
+           05 ws-math-jrprog-total     pic 9(7)v9(4).
+      *
+       01 ws-page-counters.
+           05 ws-cntr-analyst          pic 99      value 0.
+           05 ws-cntr-senprog          pic 99      value 0.
+           05 ws-cntr-prog             pic 99      value 0.
+           05 ws-cntr-jrprog           pic 99      value 0.
+           05 ws-cntr-unclass          pic 99      value 0.
+      *
+       01 ws-global-counters.
+           05 ws-global-cntr-page      pic 99      value 0.
+           05 ws-global-cntr-line      pic 99      value 0.
+           05 ws-global-cntr-analyst   pic 99      value 0.
+           05 ws-global-cntr-senprog   pic 99      value 0.
+           05 ws-global-cntr-prog      pic 99      value 0.
+           05 ws-global-cntr-jrprog    pic 99      value 0.
+
       *
       *eof constants
-       77 ws-eof-flag                  pic x       value "n".
-       77 ws-eof-Y                     pic x       value "y".
-       77 ws-eof-N                     pic x       value "n".
+       77 eof-flag                     pic x       value "n".
+       77 eof-Y                        pic x       value "y".
+       77 eof-N                        pic x       value "n".
       *
       *constants
-       77 ws-lines-per-page            pic 99      value 10.
-       77 ws-grad-code                 pic x       value "G".
-       77 ws-nongrad-code              pic x       value "N".
-       77 ws-grad-analyst-start        pic 99      value 15.
-       77 ws-grad-prog-high            pic 9       value 7.
-       77 ws-grad-prog-low             pic 9       value 2.
-       77 ws-nongrad-prog-start        pic 99      value 10.
-       77 ws-nongrad-jr-start          pic 9       value 4.
-       77 ws-cnst-analyst              pic x(7)    value "ANALYST".
-       77 ws-cnst-senprog              pic x(8)    value "SEN PROG".
-       77 ws-cnst-prog                 pic x(4)    value "PROG".
-       77 ws-cnst-jrprog               pic x(7)    value "JR PROG".
+       77 cnst-lines-per-page          pic 99      value 10.
+       77 cnst-grad-code               pic x       value "G".
+       77 cnst-nongrad-code            pic x       value "N".
+       77 cnst-grad-analyst-start      pic 99      value 15.
+       77 cnst-grad-prog-high          pic 9       value 7.
+       77 cnst-grad-prog-low           pic 9       value 2.
+       77 cnst-nongrad-prog-start      pic 99      value 10.
+       77 cnst-nongrad-jr-start        pic 9       value 4.
+       77 cnst-analyst                 pic x(7)    value "ANALYST".
+       77 cnst-senprog                 pic x(8)    value "SEN PROG".
+       77 cnst-prog                    pic x(4)    value "PROG".
+       77 cnst-jrprog                  pic x(7)    value "JR PROG".
+       77 cnst-analyst-increase        pic 99v9    value 12.8.
+       77 cnst-senprog-increase        pic 9v9     value 9.3.
+       77 cnst-prog-increase           pic 9v9     value 6.7.
+       77 cnst-jrprog-increase         pic 9v9     value 3.2.
+       77 cnst-unclass-increase        pic 9       value 0.
       *
        procedure division.
       *
@@ -181,7 +236,9 @@
            perform 100-print-report-heading.
       *
            perform 200-process-pages
-             until ws-eof-flag equals ws-eof-Y.
+             until eof-flag equals eof-Y.
+      *
+           perform 150-print-average-increases.
       *
            perform 75-close-files.
       *
@@ -196,12 +253,17 @@
       *
            read input-file
                at end
-                   move ws-eof-Y to ws-eof-flag.
+                   move eof-Y to eof-flag.
       *
        75-close-files.
       *
            close input-file
                output-file.
+      *
+       90-clear-artifacts.
+      *
+           move spaces to output-line.
+           move spaces to ws-math-store.
       *
        100-print-report-heading.
       *
@@ -211,17 +273,17 @@
       *
        125-print-page-headings.
       *
-           if ws-cntr-page > 0
-               add 1 to ws-cntr-page
-               move ws-cntr-page to ws-page-number
-
+           if ws-global-cntr-page > 0
+               add 1 to ws-global-cntr-page
+               move ws-global-cntr-page to ws-page-number
+      *
                write output-line
                  from ws-page-heading
                  after advancing page
            else
-               add 1 to ws-cntr-page
-               move ws-cntr-page to ws-page-number
-
+               add 1 to ws-global-cntr-page
+               move ws-global-cntr-page to ws-page-number
+      *
                write output-line
                  from ws-page-heading
                  after advancing 2 lines
@@ -233,71 +295,242 @@
              from ws-column-head-two
              before advancing 2 lines.
       *
-       150-clear-artifacts.
+       150-print-average-increases.
       *
-           move spaces to output-line.
-           move spaces to ws-math-store.
+           write output-line
+             from ws-increase-average-one.
+      *
+           write output-line
+             from ws-increase-average-two.
       *
        200-process-pages.
       *
+           move 0 to ws-cntr-analyst.
+           move 0 to ws-cntr-senprog.
+           move 0 to ws-cntr-prog.
+           move 0 to ws-cntr-jrprog.
+           move 0 to ws-cntr-unclass.
            perform 125-print-page-headings.
-           perform 150-clear-artifacts.
            perform 250-process-lines
-             varying ws-cntr-line from 1 by 1
-             until ws-cntr-line > ws-lines-per-page
-             or ws-eof-flag = ws-eof-Y.
+             varying ws-global-cntr-line from 1 by 1
+             until ws-global-cntr-line > cnst-lines-per-page
+             or eof-flag = eof-Y.
            perform 650-print-totals.
       *
        250-process-lines.
       *
+           perform 90-clear-artifacts.
            perform 600-create-output-line.
            perform 50-read-input-file.
       *
       *determines if an employee is a graduate
        300-is-graduate.
       *
-           if il-emp-code      =  ws-grad-code          then
-               if il-emp-years >  ws-grad-analyst-start then
-                   move ws-cnst-analyst to ws-emp-position
+           if il-emp-code      =  cnst-grad-code          then
+               if il-emp-years >  cnst-grad-analyst-start then
+                   move cnst-analyst   to ws-emp-position
+                   add  1              to ws-cntr-analyst 
+                   add  1              to ws-global-cntr-analyst 
                end-if
-               if il-emp-years <= ws-grad-analyst-start and
-                  il-emp-years >= ws-grad-prog-high     then
-                   move ws-cnst-senprog to ws-emp-position
+               if il-emp-years <= cnst-grad-analyst-start and
+                  il-emp-years >= cnst-grad-prog-high     then
+                   move cnst-senprog   to ws-emp-position
+                   add  1              to ws-cntr-senprog
+                   add  1              to ws-global-cntr-senprog
                end-if
-               if il-emp-years <  ws-grad-prog-high     and
-                  il-emp-years >  ws-grad-prog-low      then
-                   move ws-cnst-prog    to ws-emp-position
+               if il-emp-years <  cnst-grad-prog-high     and
+                  il-emp-years >  cnst-grad-prog-low      then
+                   move cnst-prog      to ws-emp-position
+                   add  1              to ws-cntr-prog
+                   add  1              to ws-global-cntr-prog
                end-if
-               if il-emp-years <= ws-grad-prog-low      then
-                   move spaces          to ws-emp-position
+               if il-emp-years <= cnst-grad-prog-low      then
+                   move spaces         to ws-emp-position
+                   add  1              to ws-cntr-unclass
                end-if
            end-if.
       *
       *determines if an employee isn't a graduate
        350-not-graduate.
       *
-           if il-emp-code      =  ws-nongrad-code       then
-               if il-emp-years >  ws-nongrad-prog-start then
-                   move ws-cnst-prog    to ws-emp-position
+           if il-emp-code      =  cnst-nongrad-code       then
+               if il-emp-years >  cnst-nongrad-prog-start then
+                   move cnst-prog      to ws-emp-position
+                   add  1              to ws-cntr-prog
+                   add  1              to ws-global-cntr-prog
                end-if
-               if il-emp-years <= ws-nongrad-prog-start and
-                  il-emp-years >  ws-nongrad-jr-start   then
-                   move ws-cnst-jrprog  to ws-emp-position
+               if il-emp-years <= cnst-nongrad-prog-start and
+                  il-emp-years >  cnst-nongrad-jr-start   then
+                   move cnst-jrprog    to ws-emp-position
+                   add  1              to ws-cntr-jrprog
+                   add  1              to ws-global-cntr-jrprog
                end-if
-               if il-emp-years <= ws-nongrad-jr-start   then
-                   move spaces          to ws-emp-position
+               if il-emp-years <= cnst-nongrad-jr-start   then
+                   move spaces         to ws-emp-position
+                   add  1              to ws-cntr-unclass
                end-if
            end-if.
+      *
+      *runs all calculation paragraphs
+       400-calculations.
+           perform 410-calculate-increase-analyst.
+           perform 420-calculate-increase-senprog.
+           perform 430-calculate-increase-prog.
+           perform 440-calculate-increase-jrprog.
+           perform 450-calculate-increase-unclass.
+           perform 460-calculate-average-increases.
+      *
+      *calculates pay increase for analysts
+       410-calculate-increase-analyst.
+      *
+           divide cnst-analyst-increase
+               by 100
+           giving ws-math-percent.
+      *
+           if ws-emp-position = cnst-analyst then
+               move cnst-analyst-increase    to ws-emp-increase-perc
+      *
+               multiply il-emp-sal
+                     by ws-math-percent
+                 giving ws-math-increase-pay rounded
+               add  ws-math-increase-pay
+                to  ws-math-analyst-total    
+               move ws-math-increase-pay     to ws-emp-increase-pay
+      *        
+               add ws-math-increase-pay
+                to il-emp-sal
+            giving ws-math-new-salary
+               move ws-math-new-salary       to ws-emp-new-salary
+           end-if.
+      *
+      *calculates pay increase for senior programmers
+       420-calculate-increase-senprog.
+      *
+           divide cnst-senprog-increase
+               by 100
+           giving ws-math-percent.
+      *
+           if ws-emp-position = cnst-senprog then
+               move cnst-senprog-increase    to ws-emp-increase-perc
+      *
+               multiply il-emp-sal
+                     by ws-math-percent
+                 giving ws-math-increase-pay rounded
+               add  ws-math-increase-pay
+                to  ws-math-senprog-total    
+               move ws-math-increase-pay     to ws-emp-increase-pay
+      *
+               add ws-math-increase-pay
+                to il-emp-sal
+            giving ws-math-new-salary
+              move ws-math-new-salary        to ws-emp-new-salary
+           end-if.
+      *          
+      *calculates pay increase for programmers
+       430-calculate-increase-prog.
+      *
+           divide cnst-prog-increase
+               by 100
+           giving ws-math-percent.
+      *
+           if ws-emp-position = cnst-prog    then
+               move cnst-prog-increase       to ws-emp-increase-perc
+      *
+               multiply il-emp-sal
+                     by ws-math-percent
+                 giving ws-math-increase-pay rounded
+               add  ws-math-increase-pay
+                to  ws-math-prog-total       
+               move ws-math-increase-pay     to ws-emp-increase-pay
+      *
+               add ws-math-increase-pay
+                to il-emp-sal
+            giving ws-math-new-salary
+              move ws-math-new-salary        to ws-emp-new-salary
+           end-if.
+      *
+      *calculates pay increase for junior programmers
+       440-calculate-increase-jrprog.
+      *
+           divide cnst-jrprog-increase
+               by 100
+           giving ws-math-percent.
+      *
+           if ws-emp-position = cnst-jrprog  then
+               move cnst-jrprog-increase     to ws-emp-increase-perc
+      *
+               multiply il-emp-sal
+                     by ws-math-percent
+                 giving ws-math-increase-pay rounded
+               add  ws-math-increase-pay
+                to  ws-math-jrprog-total     
+               move ws-math-increase-pay     to ws-emp-increase-pay
+      *
+               add ws-math-increase-pay
+                to il-emp-sal
+            giving ws-math-new-salary
+              move ws-math-new-salary        to ws-emp-new-salary
+           end-if.
+      *
+      *calculates pay increase for junior programmers
+       450-calculate-increase-unclass.
+      *
+           divide cnst-unclass-increase
+               by 100
+           giving ws-math-percent.
+      *
+           if ws-emp-position = " " then
+               move spaces                   to ws-emp-increase-perc
+      *
+               multiply il-emp-sal
+                     by ws-math-percent
+                 giving ws-math-increase-pay rounded
+               move ws-math-increase-pay     to ws-emp-increase-pay
+      *
+               add ws-math-increase-pay
+                to il-emp-sal
+            giving ws-math-new-salary
+              move ws-math-new-salary        to ws-emp-new-salary
+           end-if.
+      *
+      *calculates average salary increases for each position
+       460-calculate-average-increases.
+      *
+           divide ws-math-analyst-total
+               by ws-global-cntr-analyst
+           giving ws-math-average rounded.
+           move ws-math-average to ws-analyst-average.
+      *
+           divide ws-math-senprog-total
+               by ws-global-cntr-senprog
+           giving ws-math-average rounded.
+           move ws-math-average to ws-senprog-average.
+      *
+           divide ws-math-prog-total
+               by ws-global-cntr-prog
+           giving ws-math-average rounded.
+           move ws-math-average to ws-prog-average.
+      *
+           divide ws-math-jrprog-total
+               by ws-global-cntr-jrprog
+           giving ws-math-average rounded.
+           move ws-math-average to ws-jrprog-average.
       *
       *creates details line for output
        600-create-output-line.
       *
            perform 300-is-graduate.
            perform 350-not-graduate.
-           move il-emp-num   to ws-emp-num.
-           move il-emp-name  to ws-emp-name.
-           move il-emp-years to ws-emp-year.
-           move il-emp-sal   to ws-emp-pres-salary.
+           perform 400-calculations.
+           move ws-cntr-analyst to ws-analyst-total.
+           move ws-cntr-senprog to ws-sen-prog-total.
+           move ws-cntr-prog    to ws-prog-total.
+           move ws-cntr-jrprog  to ws-jrprog-total.
+           move ws-cntr-unclass to ws-unclassified-total.
+           move il-emp-num      to ws-emp-num.
+           move il-emp-name     to ws-emp-name.
+           move il-emp-years    to ws-emp-year.
+           move il-emp-sal      to ws-emp-pres-salary.
            write output-line
              from ws-print-line
              before advancing 1 line.
